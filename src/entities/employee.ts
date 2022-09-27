0f5b582aa006e1426f46be9a26f3bc8cd6ec6aa3
nicolasdanelon <nicolasdanelon@users.noreply.github.com>
2022-09-27 14:58:48 -0300

    1 file modified

--- src/entities/employee.ts
+++ src/entities/employee.ts
@@ -1,8 +1,27 @@
+type Pepito = {
+  days?: number[];
+  start_date?: Date;
+  end_date?: Date;
+};
+
 export class Employee {
   private name: string;
+  private between_days: any;
+  private between_dates: any;
 
-  public constructor(_name: string) {
+  public constructor(_name: string, _availability: Pepito) {
     this.name = _name;
+    this.procAvailability(_availability);
+  }
+
+  public procAvailability(a: Pepito) {
+    if (a instanceof Array) {
+      this.between_days = a; // hacer algo con a
+    }
+
+    if (a instanceof Object) {
+      this.between_dates = a; // hacer algo con a
+    }
   }
 
   public getName(): string {
